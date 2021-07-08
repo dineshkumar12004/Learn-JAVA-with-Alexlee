5eee246178ffa13d8ea69b58c829b2dc60d33c97
Dinesh kumar <dineshkumar12004@gmail.com>
2021-07-08 12:25:37 +0530

    ArrayList program

---
+++ Basics/ArrayListsProj.java
@@ -0,0 +1,14 @@
+package Basics;
+
+import java.util.ArrayList;
+
+public class ArrayListsProj {
+    public static void main(String[] args) {
+        ArrayList<String> fruitList = new ArrayList<String>();
+        fruitList.add("Mango");
+        fruitList.add("Apple");
+        fruitList.add("strawberry");
+        System.out.println(fruitList);
+        System.out.println(fruitList.size());
+    }
+}

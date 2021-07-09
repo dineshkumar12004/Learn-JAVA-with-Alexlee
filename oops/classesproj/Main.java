f78c8ca7654ccb75cbba6b63caecd3150fb3a5e5
Dinesh kumar <dineshkumar12004@gmail.com>
2021-07-09 11:36:47 +0530

    classes in java

---
+++ oops/classesproj/Main.java
@@ -0,0 +1,13 @@
+package classesproj;
+
+public class Main {
+    public static void main(String[] args) {
+        ourProj c = new ourProj();
+        ourProj d = new ourProj();
+
+        System.out.println(c.chars);
+        System.out.println(d.x);
+        c.printHi();
+
+    }
+}
---
+++ oops/classesproj/ourProj.java
@@ -0,0 +1,10 @@
+package classesproj;
+
+public class ourProj {
+    int x =2;
+    char [] chars = {'a', 'b', 'c'};
+
+    void printHi(){
+        System.out.println("Hi !");
+    }
+}

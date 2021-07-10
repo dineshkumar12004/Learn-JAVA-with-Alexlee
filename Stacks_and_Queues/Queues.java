e9f043c3256b9771ffbeac27edc0140251876b2e
Dinesh kumar <dineshkumar12004@gmail.com>
2021-07-10 11:42:42 +0530

    Stacks and queues in java

--- Stacks_and_Queues/Queues.java
+++ Stacks_and_Queues/Queues.java
@@ -1,7 +1,20 @@
-class Queues{
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Queues {
     public static void main(String[] args) {
         System.out.println("This is Queues ");
 
-        
+        Queue<String> bbqLine = new LinkedList<String>();
+
+        bbqLine.add("Jack");
+        bbqLine.add("Tyrant");
+        bbqLine.add("Karen");
+
+        System.out.println(bbqLine.poll());
+        System.out.println(bbqLine.peek());
+
+        System.out.println(bbqLine);
+
     }
 }

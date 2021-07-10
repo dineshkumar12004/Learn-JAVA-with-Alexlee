5d3ef49c61b2efc785f4c996036df37b47f26ba0
Dinesh kumar <dineshkumar12004@gmail.com>
2021-07-10 11:23:02 +0530

    Satcks and Queues in java

---
+++ Stacks_and_Queues/stacksAreFun.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class stacksAreFun {
+    public static void main(String[] args) {
+        System.out.println("Stacks in Java");
+
+        Stack<String> games = new Stack<String>();
+
+        games.add("COD");
+        games.add("Guitar hero");
+        games.add("Super monkey ball");
+
+        System.out.println(games.pop());
+        System.out.println(games.peek());
+        System.out.println(games);
+
+
+
+
+
+        
+
+
+        
+
+    }
+}

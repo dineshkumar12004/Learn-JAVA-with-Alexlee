b225db0a439e7d1e9c8fdd4eee22ee3ebbcb3e8f
Dinesh kumar <dineshkumar12004@gmail.com>
2021-07-11 08:59:07 +0530

    static and main methods

---
+++ oops/static and main method/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        sayHi("Sayonara");
+    }
+
+    public static void sayHi(String s) {
+        System.out.println(s);
+    }
+}
---
+++ oops/static and main method/staticKeyword.java
@@ -0,0 +1,9 @@
+class staticKeyword{
+     static int a = 0;
+    public static void main(String[] args) {
+        System.out.println("hi");
+        int b = 1;
+
+        System.out.println(a);
+    }
+}

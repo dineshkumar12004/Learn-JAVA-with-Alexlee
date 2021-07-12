06889909bbc28a9183dc2c039ad88bb333ee3723
Dinesh kumar <dineshkumar12004@gmail.com>
2021-07-12 10:17:03 +0530

    Encapsulation in java

---
+++ oops/encapsulation/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        student s = new student();
+        s.setName("Ginne");
+        s.setAge(20);
+
+        System.out.println(s.getName());
+
+        System.out.println(s.getAge());
+    }
+}
---
+++ oops/encapsulation/student.java
@@ -0,0 +1,22 @@
+public class student {
+
+    private String name;
+    private int age;
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public String  getName() {
+        return name;
+    }
+
+    public void  setAge(int newAge) {
+        age = newAge;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+}

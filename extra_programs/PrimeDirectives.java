acb8d67b0aff659971b5afb021ae22597d85ef6a
dineshkumar12004 <dineshkumar12004@gmail.com>
2021-07-31 14:30:34 +0530

    Improved Readability

--- extra_programs/PrimeDirectives.java
+++ extra_programs/PrimeDirectives.java
@@ -1,44 +1,43 @@
+
 // Import statement:
 import java.util.ArrayList;
 
 class PrimeDirective {
-  
-  // Add your methods here:
-  public boolean isPrime(int number){
-    if(number == 2){
-      return true;
-    }else if(number < 2){
-      return false;
-    }else{
-      for(int i =2; i < number; i++){
-      if(number % i == 0){
-        return false;
-      }
+
+    // Add your methods here:
+    public boolean isPrime(int number) {
+        if (number == 2) {
+            return true;
+        } else if (number < 2) {
+            return false;
+        } else {
+            for (int i = 2; i < number; i++) {
+                if (number % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
     }
-    return true;
-  }
-  }
 
-  public ArrayList<Integer> onlyPrimes(int[] numbers){
-    ArrayList<Integer> primes = new ArrayList<Integer>();
-    for(int number:numbers){
-      if(isPrime(number)){
-      primes.add(number);
-      }
-    }    
-    
-    return primes;
-  }
+    public ArrayList<Integer> onlyPrimes(int[] numbers) {
+        ArrayList<Integer> primes = new ArrayList<Integer>();
+        for (int number : numbers) {
+            if (isPrime(number)) {
+                primes.add(number);
+            }
+        }
 
-  
-  public static void main(String[] args) {
+        return primes;
+    }
 
-    PrimeDirective pd = new PrimeDirective();
-    int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
-    //System.out.println(pd.isPrime(7));
-    System.out.println(pd.onlyPrimes(numbers));
+    public static void main(String[] args) {
 
-    
-  }  
+        PrimeDirective pd = new PrimeDirective();
+        int[] numbers = { 6, 29, 28, 33, 11, 100, 101, 43, 89 };
+        // System.out.println(pd.isPrime(7));
+        System.out.println(pd.onlyPrimes(numbers));
+
+    }
 
 }

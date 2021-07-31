81b457d4a96edd6f7290576b5058d1acf7f19525
dineshkumar12004 <dineshkumar12004@gmail.com>
2021-07-31 14:26:27 +0530

    Prime numbers in a ArrayList

---
+++ extra_programs/PrimeDirectives.java
@@ -0,0 +1,44 @@
+// Import statement:
+import java.util.ArrayList;
+
+class PrimeDirective {
+  
+  // Add your methods here:
+  public boolean isPrime(int number){
+    if(number == 2){
+      return true;
+    }else if(number < 2){
+      return false;
+    }else{
+      for(int i =2; i < number; i++){
+      if(number % i == 0){
+        return false;
+      }
+    }
+    return true;
+  }
+  }
+
+  public ArrayList<Integer> onlyPrimes(int[] numbers){
+    ArrayList<Integer> primes = new ArrayList<Integer>();
+    for(int number:numbers){
+      if(isPrime(number)){
+      primes.add(number);
+      }
+    }    
+    
+    return primes;
+  }
+
+  
+  public static void main(String[] args) {
+
+    PrimeDirective pd = new PrimeDirective();
+    int[] numbers = {6, 29, 28, 33, 11, 100, 101, 43, 89};
+    //System.out.println(pd.isPrime(7));
+    System.out.println(pd.onlyPrimes(numbers));
+
+    
+  }  
+
+}
